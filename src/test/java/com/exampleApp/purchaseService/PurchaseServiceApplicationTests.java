a0b67b1a9cc66d7ba967c58afcd66c12d3055fb7
Felipe Sánchez Soberanis <felipesaso2000@gmail.com>
2022-08-04 10:34:17 -0500

    Initial commit
    == Problem ==
    
    == Solution ==
    
    == Note ==

---
+++ src/test/java/com/exampleApp/purchaseService/PurchaseServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package com.exampleApp.purchaseService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurchaseServiceApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
